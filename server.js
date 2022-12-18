893fa1a67be565cd275497383583d9601711848b
lan-mak <lan-mak@ya.ru>
2022-12-18 23:06:42 +0300

    upd

--- server.js
+++ server.js
@@ -1,19 +1,50 @@
 const http = require('http');
+const fs = require('fs');
 const Koa = require('koa');
+const {koaBody} = require('koa-body');
 
 const appServer = new Koa();
 
-appServer.use((ctx, next) => {
-  console.log(ctx)
+const base = fs.readFileSync('base.json')
+const baseTickets = JSON.parse(base)
 
-  ctx.response.body = 'ответ получен'
-  next()
-})
+appServer.use(koaBody({
+  urlencoded: true,
+}));
 
-appServer.use((ctx) => {
-  console.log('ответ полученwwww')
+appServer.use( ctx => {
+  const { method, id } = ctx.request.query;
 
-})
+  switch (method) {
+      case 'allTickets':
+          ctx.response.body = baseTickets;
+          return;
+
+      case 'ticketById': 
+          baseTickets.forEach(item => {
+            if (item.id === +id) {
+              ctx.response.body = item;
+            }
+          })
+          return;
+
+      case 'createTicket': 
+          if (ctx.method === 'POST') {
+            ctx.request.body.id = baseTickets[baseTickets.length-1].id + 1
+            baseTickets.push(ctx.request.body)
+
+            fs.writeFile('base.json', JSON.stringify(baseTickets), function(error){
+              if(error) throw error; // если возникла ошибка
+              console.log("Запись файла base.json завершена");
+            })
+          }
+          return
+
+      default:
+          ctx.response.status = 404;
+          return;
+  }
+});
 
 const server = http.createServer(appServer.callback())
 
@@ -23,9 +54,7 @@ const port = 2121;
 server.listen(port, (err) => {
   if (err) {
     console.log(err);
-
     return;
   }
-
-  console.log('server work! port: ' + port)
+  console.log('Сервер запущен! \nАдрес localhost:' + port)
 })
